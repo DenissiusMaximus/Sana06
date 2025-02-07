13a1f1fd092e63b46f5dd80d66fb22b4cc582df0
KramarDenys <ipz243_kdo@student.ztu.edu.ua>
2025-02-07 21:41:03 +0200

    Create

---
+++ WebApplication2/Controllers/NewController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication2.Controllers;
+
+public class NewController : Controller
+{
+    // GET
+    public IActionResult More()
+    {
+        return View();
+    }
+}
